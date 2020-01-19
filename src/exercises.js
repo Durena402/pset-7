a6b11e6df9b4b12dd57ae1b3b1a3d8256185e471
Daneliz Urena <durena402@gmail.com>
2020-01-18 23:42:49 -0500

    Excercise 6 progress

--- src/exercises.js
+++ src/exercises.js
@@ -16,27 +16,64 @@ function commonEnd(a, b) {
 }
 
 function endsMeet(values, n) {
-  if ( values === null || values === undefined || values.length < n || Number.isInteger(n)){
-    return [];
-  }
-  
+  let array = [];
 
+  if (!values || values.length < n || !Number.isInteger(n) || n < 0) {
+    return array;
+  } else {
+    if (n === 0) {
+      return values;
+    }
+
+      for (let i = 0; i < n; i++) {
+          array.push(values[i]);
+      }
+
+      for (let i = n; i > 0; i--) {
+          array.push(values[values.length - i]);
+      }
+      return array;
+    }
 }
 
 function difference(numbers) {
-  // write your code here
+  if (numbers == undefined||numbers < 1||numbers.some(isNaN)) {
+    return undefined;
+  } else {
+    let largestNum = Math.max.apply(Math, numbers);
+    let smallestNum = Math.min.apply(Math, numbers);
+    return largestNum - smallestNum;
+  }
 }
 
 function max(number) {
-  // write your code here
-}
+  if (number === undefined || number === null || number.length < 3 || number.length % 2 === 0 || number.some(isNaN)){
+    return undefined;
+  } else {
+    let first = number[0];
+    let middle = number[Math.floor((number.length - 1) / 2)];
+    let last = number[number.length - 1];
+    let output = [first, middle, last];
 
+    return Math.max.apply(Math, output);
+  }
+}
 function middle(values) {
-  // write your code here
+  if (values === undefined || values === null || values.length < 3 || values.length % 2 === 0){
+    return [];
+  } else {
+    let middleOne = values [Math.floor((values.length - 1) / 2) - 1];
+    let middleTwo = values[(values.length - 1) / 2];
+
+    return [middleOne, middleTwo];
+  }
 }
 
 function increasing(numbers) {
-  // write your code here
+if (numbers === undefined || numbers === null || numbers.length < 3 || numbers.some(isNaN) || !Number.isInteger())
+  return false;
+} else {
+
 }
 
 function everywhere(values, x) {
